c88be74042d15aae7173464e9207dce55d77583b
Trevor Manz <trevor.j.manz@gmail.com>
2023-01-19 21:58:58 -0500

    fix title

--- docs/src/util.ts
+++ docs/src/util.ts
@@ -1,9 +1,5 @@
 import * as CONFIG from "./config";
 
 export function getTitle(frontmatter: { title?: string } = {}) {
-	let title = CONFIG.SITE.title;
-	if (frontmatter?.title) {
-		title = `${frontmatter.title} | ${title}`;
-	}
-	return title;
+	return frontmatter?.title ?? CONFIG.SITE.title;
 }
