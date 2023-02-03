76ba758380e545026fc26b635b774a720e2c5883
Talley Lambert <talley.lambert@gmail.com>
2023-02-03 14:50:45 -0500

    wip: decorator example

---
+++ anywidget/_decorator.py
@@ -0,0 +1,50 @@
+from typing import Any, Callable, Iterable, TypeVar
+from psygnal import evented
+from dataclasses import dataclass, is_dataclass
+from ._displayable import _Displayable, open_comm
+
+T = TypeVar("T")
+
+
+# TODO: this will move to psygnal
+def _is_evented(cls: Any) -> bool:
+    return getattr(cls.__setattr__, "__module__", "").startswith("psygnal")
+
+
+def anywidget(
+    _cls: T | None = None, *, esm: str = "", css: str | None = None
+) -> Callable[[T], T]:
+    def _get_state(obj: object, include: Iterable[str] | None = None) -> dict:
+        state = _Displayable._get_state(obj, include)
+        if include is None or "_esm" in include:
+            state["_esm"] = esm
+        if (include is None or "_css" in include) and css:
+            state["_css"] = css
+        return state
+
+    def anywidget_decorator(cls: T) -> T:
+        if not isinstance(cls, type):  # pragma: no cover
+            raise TypeError("anywidget decorator can only be used on classes")
+
+        if not is_dataclass(cls):
+            cls = dataclass(cls)
+        cls = type(
+            f"{cls.__name__}Widget",
+            (cls, _Displayable),
+            {"_get_state": _get_state},
+        )
+        if not _is_evented(cls):
+            cls = evented(cls, events_namespace="events")
+
+        original_init = cls.__init__
+
+        def __comm_init__(self: _Displayable, *args: Any, **kwargs: Any) -> None:
+            original_init(self, *args, **kwargs)
+            self._comm = open_comm()
+            self._comm.on_msg(self._handle_msg)
+            self._send_state()
+
+        cls.__init__ = __comm_init__
+        return cls
+
+    return anywidget_decorator(_cls) if _cls is not None else anywidget_decorator
--- anywidget/_displayable.py
+++ anywidget/_displayable.py
@@ -1,7 +1,11 @@
-from typing import Any, Container, Generic, TypeVar, cast, TypedDict, Literal
+from typing import Any, Iterable, TypeVar, cast, TypedDict, Literal
 from ipykernel.comm import Comm
 from psygnal import EventedModel, EmissionInfo
 from pydantic import PrivateAttr
+from dataclasses import is_dataclass, asdict
+
+_BINARY_TYPES = (memoryview, bytearray, bytes)
+T = TypeVar("T", list, dict, tuple)
 
 
 class UpdateData(TypedDict):
@@ -14,12 +18,9 @@ class RequestStateData(TypedDict):
     method: Literal["request_state"]
 
 
-ContentT = TypeVar("ContentT")
-
-
-class CustomData(TypedDict, Generic[ContentT]):
+class CustomData(TypedDict):
     method: Literal["custom"]
-    content: ContentT
+    content: dict  # Generic[ContentT] ... but only works with TypedDict in py311
 
 
 class JupyterWidgetContent(TypedDict):
@@ -37,38 +38,69 @@ class CommMessage(TypedDict):
     buffers: list[memoryview]
 
 
-class Displayable(EventedModel):
+_PROTOCOL_VERSION_MAJOR = 2
+_PROTOCOL_VERSION_MINOR = 1
+
+
+def open_comm():
+    version = f"{_PROTOCOL_VERSION_MAJOR}.{_PROTOCOL_VERSION_MINOR}.0"
+    return Comm(
+        target_name="jupyter.widget",
+        metadata={"version": version},
+        buffers=[],
+        data={
+            # temporarily hard-code the model name and module
+            "state": {
+                "_model_module": "anywidget",
+                "_model_name": "AnyModel",
+                "_model_module_version": "0.1.0",
+                "_view_count": None,
+                "_view_module": "anywidget",
+                "_view_name": "AnyView",
+                "_view_module_version": "0.1.0",
+            }
+        },
+    )
+
+
+class _Displayable:
     _JUPYTER_MIME = "application/vnd.jupyter.widget-view+json"
     _MAX_REPR_SIZE = 110
-    _PROTOCOL_VERSION_MAJOR = 2
-    _PROTOCOL_VERSION_MINOR = 1
-    _comm: Comm = PrivateAttr(default=None)
+    _comm: Comm
 
-    esm: str
+    def _get_state(self, include: Iterable[str] | None = None) -> dict:
+        """Serialize this object into a state dict.
+
+        Default implementation works for dataclasses, but will likely need to be
+        reimplemented for anything else.
+
+        Parameters
+        ----------
+        include : Iterable[str], optional
+            A set of attribute names to include in the state, by default None (all).
+        """
+        if is_dataclass(self):
+            state = asdict(self)
+        else:
+            state = getattr(self, '__dict__', None)
+            if state is None:
+                raise TypeError(f"Cannot serialize {self!r} to a state dict")
+        if include is not None:
+            state = {k: v for k, v in state.items() if k in set(include)}
+        return state
+
+    def _set_state(self, state: dict) -> None:
+        # MAY be overriden by subclasses... this default works for, e.g.
+        # dataclasses, pydantic, attrs...
+        for key, val in state.items():
+            setattr(self, key, val)
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
-        version = f"{self._PROTOCOL_VERSION_MAJOR}.{self._PROTOCOL_VERSION_MINOR}.0"
-        self._comm = Comm(
-            target_name="jupyter.widget",
-            metadata={"version": version},
-            buffers=[],
-            data={
-                "state": {
-                    "_model_module": "anywidget",
-                    "_model_name": "AnyModel",
-                    "_model_module_version": "0.1.0",
-                    "_view_count": None,
-                    "_view_module": "anywidget",
-                    "_view_name": "AnyView",
-                    "_view_module_version": "0.1.0",
-                }
-            },
-        )
+
+        self._comm = open_comm()
         self._comm.on_msg(self._handle_msg)
-        self.send_state()
-        # send state on ANY change
-        self.events.connect(self._on_event)
+        self._send_state()
 
     def _handle_msg(self, msg: CommMessage):
         """Called when a msg is received from the front-end"""
@@ -79,11 +111,11 @@ class Displayable(EventedModel):
                 state = data["state"]
                 if "buffer_paths" in data:
                     _put_buffers(state, data["buffer_paths"], msg["buffers"])
-                self.set_state(state)
+                self._set_state(state)
 
         # Handle a state request.
         elif data["method"] == "request_state":
-            self.send_state()
+            self._send_state()
 
         # Handle a custom msg from the front-end.
         elif data["method"] == "custom":
@@ -94,27 +126,17 @@ class Displayable(EventedModel):
             # TODO: log unknown message or raise an Exception?
             ...
 
-    def set_state(self, state):
-        for key, val in state.items():
-            setattr(self, key, val)
-
     def _handle_custom_msg(self, content: Any, buffers: list[memoryview]):
         # TODO: handle custom callbacks
         # https://github.com/jupyter-widgets/ipywidgets/blob/6547f840edc1884c75e60386ec7fb873ba13f21c/python/ipywidgets/ipywidgets/widgets/widget.py#L662
         ...
 
-    def _on_event(self, event: EmissionInfo):
-        """Called whenever the python model changes"""
-        self.send_state({event.signal.name})
-
-    def send_state(self, include: Container[str] | None = None):
+    def _send_state(self, include: Iterable[str] | None = None) -> None:
         """Send state or a part of it"""
-        state = self.dict(include=include)  # type: ignore
+        state = self._get_state(include)
         if not state:
             return
 
-        if "esm" in state:
-            state["_esm"] = state.pop("esm")  # can fix... cause pydantic doesn't want _
         # if self._property_lock: ... # TODO
         state, buffer_paths, buffers = _remove_buffers(state)
         if self._comm.kernel is not None:
@@ -128,15 +150,31 @@ class Displayable(EventedModel):
         return {
             "text/plain": plaintext,
             self._JUPYTER_MIME: {
-                "version_major": self._PROTOCOL_VERSION_MAJOR,
-                "version_minor": self._PROTOCOL_VERSION_MINOR,
+                "version_major": _PROTOCOL_VERSION_MAJOR,
+                "version_minor": _PROTOCOL_VERSION_MINOR,
                 "model_id": self._comm.comm_id,
             },
         }
 
 
-_BINARY_TYPES = (memoryview, bytearray, bytes)
-T = TypeVar("T", list, dict, tuple)
+class Displayable(_Displayable, EventedModel):
+    _comm: Comm = PrivateAttr(None)
+
+    def __init__(_model_self_, **data: Any) -> None:
+        super().__init__(**data)
+        # send state on ANY change
+        _model_self_.events.connect(_model_self_._on_event)
+
+    def _on_event(self, event: EmissionInfo):
+        """Called whenever the python model changes"""
+        self._send_state({event.signal.name})
+
+    def _get_state(self, include: Iterable[str] | None = None) -> dict:
+        state = self.dict(include=set(include) if include else None)
+
+        if "esm" in state:  # TODO
+            state["_esm"] = state.pop("esm")  # can fix... cause pydantic doesn't want _
+        return state
 
 
 def _separate_buffers(substate: T, path: list, buffer_paths: list, buffers: list) -> T:
