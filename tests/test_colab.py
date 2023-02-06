a7f5e5bf5db954502dbb27e6480d66e58dd06581
Trevor Manz <trevor.j.manz@gmail.com>
2023-02-05 20:57:05 -0500

    use pytest fixtures

--- tests/test_colab.py
+++ tests/test_colab.py
@@ -1,28 +1,40 @@
+import pytest
 import collections
-import unittest.mock as mock
+from unittest.mock import patch, MagicMock
 import sys
 
 import anywidget
+
+import IPython.display
 
 
 def fake_module(**kwargs: dict):
     return collections.namedtuple("module", *kwargs.keys())(**kwargs)
 
 
-def test_enables_widget_manager_in_colab():
-    fn = mock.Mock()
-    fake = fake_module(enable_custom_widget_manager=fn)
-    with mock.patch.dict(sys.modules, {"google.colab.output": fake}):
-        anywidget.AnyWidget()
-        anywidget.AnyWidget()
-        assert fn.assert_called_once
+@pytest.fixture
+def mock_colab():
+    enable_custom_widget_manager = MagicMock()
+    mock_module = fake_module(enable_custom_widget_manager=enable_custom_widget_manager)
+    with patch.dict(sys.modules, {"google.colab.output": mock_module}):
+        yield mock_module
 
 
-def test_adds_ipython_display_in_colab():
-    fake = fake_module(enable_custom_widget_manager=lambda: None)
-    with mock.patch.dict(sys.modules, {"google.colab.output": fake}):
-        w = anywidget.AnyWidget()
-        assert hasattr(w, "_ipython_display_")
+def test_enables_widget_manager_in_colab(
+    monkeypatch: pytest.MonkeyPatch,
+    mock_colab: MagicMock,
+):
+    mock_display = MagicMock()
+    monkeypatch.setattr(IPython.display, "display", mock_display)
+
+    anywidget.AnyWidget()
+    anywidget.AnyWidget()
+    w = anywidget.AnyWidget()
+    assert mock_colab.enable_custom_widget_manager.assert_called_once
+
+    assert hasattr(w, "_ipython_display_")
+    w._ipython_display_()
+    assert mock_display.assert_called_once
 
 
 def test_default_no_ipython_display():
