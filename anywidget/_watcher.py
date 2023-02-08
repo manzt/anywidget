99a3e1679480b75ec266678ee3b0f0e74c816496
Trevor Manz <trevor.j.manz@gmail.com>
2023-02-08 13:58:57 -0500

    add _handlers_changed method

--- anywidget/_watcher.py
+++ anywidget/_watcher.py
@@ -24,19 +24,7 @@ class LiveWatcher:
 
         self._handlers[file].add(handler)
 
-        # Our background thread watches directories for changes, so we
-        # consolidate all file parents (directories) that we need to watch.
-        should_be_watching = set(p.parent for p in self._handlers.keys())
-
-        # No need to teardown/restart a thread if we are already watching
-        # the right directories.
-        if self._watching != should_be_watching:
-            self._stop_background_thread()
-
-        # (Re)start the watcher thread if we don't have one
-        if self._background_thread is None:
-            self._start_background_thread()
-
+        self._handlers_changed()
         return self
 
     def unwatch(self, file: str | pathlib.Path, handler: FileChangeHandler):
@@ -51,6 +39,13 @@ class LiveWatcher:
 
         # Clean up the file and check what we should be watching
         self._handlers.pop(file)
+
+        self._handlers_changed()
+        return self
+
+    def _handlers_changed(self):
+        # Our background thread watches directories for changes, so we
+        # consolidate all file parents (directories) that we need to watch.
         should_be_watching = set(p.parent for p in self._handlers.keys())
 
         # No need to teardown/restart a thread if we are already watching
@@ -63,17 +58,16 @@ class LiveWatcher:
         if len(self._handlers) > 0 and self._background_thread is None:
             self._start_background_thread()
 
-        return self
-
     def _start_background_thread(self):
         if self._background_thread is not None:
             return self
 
         self._stop_event = threading.Event()
-        self._watching = set(p.parent for p in self._handlers.keys())
 
         def run():
             from watchfiles import watch, Change
+
+            self._watching = set(p.parent for p in self._handlers.keys())
 
             for changes in watch(*self._watching, stop_event=self._stop_event):
                 for change, path in changes:
@@ -82,6 +76,8 @@ class LiveWatcher:
                         contents = path.read_text()
                         for handler in self._handlers[path]:
                             handler(contents)
+
+            self._watching.clear()
 
         self._background_thread = threading.Thread(target=run, daemon=True)
         self._background_thread.start()
@@ -99,7 +95,6 @@ class LiveWatcher:
         finally:
             self._stop_event = None
             self._background_thread = None
-            self._watching.clear()
 
         return self
 
