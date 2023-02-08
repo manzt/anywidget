46baff713c6d83cd7713661923b27ea8a49cec28
Trevor Manz <trevor.j.manz@gmail.com>
2023-02-08 11:27:45 -0500

    reset/run _Watcher in thread

--- anywidget/_watcher.py
+++ anywidget/_watcher.py
@@ -3,65 +3,91 @@ from collections import defaultdict
 
 import pathlib
 import threading
-from typing import TYPE_CHECKING, Callable
+from typing import Callable, Mapping
+
+FileChangeHandler = Callable[[str], None]
+Handlers = Mapping[pathlib.Path, set[FileChangeHandler]]
 
 
-if TYPE_CHECKING:
-    pass
+class _Watcher:
+    _watching: None | set[pathlib.Path] = None
+
+    def __init__(self, handlers: Handlers):
+        self._handlers = handlers
+        self._stop_event = threading.Event()
+
+    @property
+    def watching(self):
+        return self._watching
+
+    def run(self):
+        from watchfiles import watch, Change
+
+        self._watching = set(p.parent for p in self._handlers.keys())
+
+        for changes in watch(*self._watching, stop_event=self._stop_event):
+            for change, path in changes:
+                path = pathlib.Path(path)
+                if path in self._handlers and change != Change.deleted:
+                    contents = path.read_text()
+                    for handler in self._handlers[path]:
+                        handler(contents)
+
+        self._watching = None
+
+    def stop(self):
+        self._stop_event.set()
+        self._stop_event = threading.Event()
 
 
 class BackgroundWatcher:
-    _thread: None | tuple[threading.Thread, threading.Event] = None
-    _handlers: defaultdict[pathlib.Path, set[Callable[[str], None]]]
-    _dirs: set[pathlib.Path]
+    _watcher: _Watcher | None = None
+    _watcher_thread: threading.Thread | None = None
 
     def __init__(self):
-        self._handlers = defaultdict(set)
-        self._dirs = set()
+        self._handlers: Handlers = defaultdict(set)
 
-    def watch(self, file: str | pathlib.Path, handler: Callable[[str], None]):
+    def watch(self, file: str | pathlib.Path, handler: FileChangeHandler):
         file = pathlib.Path(file).absolute()
+        assert file.is_file(), "Can only add individual files to watch"
 
         self._handlers[file].add(handler)
-        new_dirs = set(p.parent for p in self._handlers.keys())
 
-        if new_dirs == self._dirs:
+        # Our background thread watches directories for changes, so we
+        # consolidate all file parents (directories) that we need to watch.
+        should_be_watching = set(p.parent for p in self._handlers.keys())
+
+        # No need to teardown/restart a thread if we are already watching
+        # the right directories.
+        if self._watcher and self._watcher.watching == should_be_watching:
             return self
 
-        self._dirs = new_dirs
-        self.start()
+        # Update what we should be watching and (re)start the thread.
+        self.start()  # tearsdown existing thread if needed
         return self
 
     def start(self):
-        from watchfiles import watch, Change
-
-        if self._thread:
+        # kill an existing thread
+        if self._watcher_thread:
             self.stop()
-
-        stop_event = threading.Event()
-
-        def run():
-            for changes in watch(*self._dirs, stop_event=stop_event):
-                for change, path in changes:
-                    path = pathlib.Path(path)
-                    if path in self._handlers and change != Change.deleted:
-                        contents = path.read_text()
-                        for handler in self._handlers[path]:
-                            handler(contents)
-
-        thread = threading.Thread(target=run, daemon=True)
-        thread.start()
-        self._thread = thread, stop_event
-
+        self._watcher = _Watcher(self._handlers)
+        self._watcher_thread = threading.Thread(target=self._watcher.run, daemon=True)
+        self._watcher_thread.start()
         return self
 
     def stop(self):
-        if self._thread is None:
-            return
-        thread, stop_event = self._thread
-        stop_event.set()
-        thread.join()
-        self._thread = None
+        if self._watcher_thread is None:
+            return self
+        assert self._watcher is not None
+
+        try:
+            # queue exit event and wait for thread to terminate
+            self._watcher.stop()
+            self._watcher_thread.join()
+        finally:
+            self._watcher = None
+            self._watcher_thread = None
+
         return self
 
 
