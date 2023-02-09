2a3f82dcd7df080a481ab6aed997d599ac9ab369
Trevor Manz <trevor.j.manz@gmail.com>
2023-02-09 15:48:40 -0500

    add traitlets/descriptor tests

--- tests/test_file_contents.py
+++ tests/test_file_contents.py
@@ -1,11 +1,11 @@
-from collections import deque
 import pathlib
+import time
+from collections import deque
 from unittest.mock import MagicMock, Mock
 
 import pytest
 import watchfiles
 from watchfiles import Change
-import time
 
 from anywidget._file_contents import FileContents
 
@@ -18,8 +18,7 @@ def test_file_contents_no_watch(
 
     CONTENTS = "hello, world"
     path = tmp_path / "foo.txt"
-    with open(path, mode="w") as f:
-        f.write(CONTENTS)
+    path.write_text(CONTENTS)
 
     contents = FileContents(path, start_thread=False)
 
@@ -31,8 +30,7 @@ def test_file_contents_no_watch(
 def test_file_contents_deleted(monkeypatch: pytest.MonkeyPatch, tmp_path: pathlib.Path):
     CONTENTS = "hello, world"
     path = tmp_path / "foo.txt"
-    with open(path, mode="w") as f:
-        f.write(CONTENTS)
+    path.write_text(CONTENTS)
 
     def mock_file_events():
         changes = set()
@@ -58,16 +56,13 @@ def test_file_contents_deleted(monkeypatch: pytest.MonkeyPatch, tmp_path: pathli
 def test_file_contents_changed(monkeypatch: pytest.MonkeyPatch, tmp_path: pathlib.Path):
     CONTENTS = "hello, world"
     path = tmp_path / "foo.txt"
-    with open(path, mode="w") as f:
-        f.write(CONTENTS)
-
+    path.write_text(CONTENTS)
     contents = FileContents(path, start_thread=False)
 
     NEW_CONTENTS = "blah"
 
     def mock_file_events():
-        with open(path, mode="w") as f:
-            f.write(NEW_CONTENTS)
+        path.write_text(NEW_CONTENTS)
         changes = set()
         changes.add((Change.modified, str(path)))
         yield changes
@@ -115,8 +110,7 @@ def test_background_file_contents(
 ):
     CONTENTS = "hello, world"
     path = tmp_path / "foo.txt"
-    with open(path, mode="w") as f:
-        f.write(CONTENTS)
+    path.write_text(CONTENTS)
 
     contents = FileContents(path, start_thread=False)
     mock_changed = MagicMock()
@@ -129,8 +123,7 @@ def test_background_file_contents(
 
     def mock_file_events():
         # write to file
-        with open(path, mode="w") as f:
-            f.write(NEW_CONTENTS)
+        path.write_text(NEW_CONTENTS)
         changes = set()
         changes.add((Change.modified, str(path)))
         yield changes
@@ -139,8 +132,7 @@ def test_background_file_contents(
         changes.add((Change.deleted, str(path)))
         yield changes
         # "re-create the file"
-        with open(path, mode="w") as f:
-            f.write(NEW_CONTENTS)
+        path.write_text(NEW_CONTENTS)
         changes = set()
         changes.add((Change.modified, str(path)))
 
