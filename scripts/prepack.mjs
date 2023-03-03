f1e8c5e332eb4120428f8997c9ff9b469b79fee2
Trevor Manz <trevor.j.manz@gmail.com>
2023-03-02 20:24:27 -0500

    chore: reorganize JS/TS source

--- scripts/prepack.mjs
+++ scripts/prepack.mjs
@@ -7,12 +7,12 @@ import * as esbuild from "esbuild";
 
 let __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
-let root = path.join(__dirname, "..");
-let dist = path.join(root, "dist");
+let src = path.join(__dirname, "..", "src");
+let dist = path.join(__dirname, "..", "dist");
 
 // bundle the widget
 await esbuild.build({
-	entryPoints: [path.join(root, "src", "index.js")],
+	entryPoints: [path.join(src, "index.js")],
 	bundle: true,
 	format: "esm",
 	outfile: path.join(dist, "index.js"),
@@ -20,13 +20,13 @@ await esbuild.build({
 
 // Just copy over the ESM
 await fs.copyFile(
-	path.join(root, "vite-plugin.js"),
+	path.join(src, "vite-plugin.js"),
 	path.join(dist, "vite.mjs"),
 );
 
 // Transform the vite plugin to CJS
 await esbuild.build({
-	entryPoints: [path.join(root, "vite-plugin.js")],
+	entryPoints: [path.join(src, "vite-plugin.js")],
 	format: "cjs",
 	outfile: path.join(dist, "vite.cjs"),
 });
@@ -34,7 +34,7 @@ await esbuild.build({
 
 // Copy over the types and create empty files
 await fs.copyFile(
-	path.join(root, "types.d.ts"),
+	path.join(src, "types.d.ts"),
 	path.join(dist, "types.d.ts"),
 );
 await fs.writeFile(path.join(dist, "types.mjs"), "");
