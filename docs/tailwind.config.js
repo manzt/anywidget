0d53e54c4565a084e2c04c3e92ff341bb8c9816d
Trevor Manz <trevor.j.manz@gmail.com>
2023-06-05 13:42:53 -0400

    migrate to ESM configs

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,7 +1,7 @@
-const colors = require("tailwindcss/colors");
+import * as colors from "tailwindcss/colors.js";
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
