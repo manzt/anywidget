4ce54297f011e24b11a92e20ebb8bc00926b4f96
Trevor Manz <trevor.j.manz@gmail.com>
2023-06-05 14:01:17 -0400

    switch back to CJS tailwind

--- docs/tailwind.config.cjs
+++ docs/tailwind.config.cjs
@@ -1,7 +1,7 @@
-import * as colors from "tailwindcss/colors.js";
+const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
