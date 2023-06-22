1a8d370e36a69f0813b8eabd3903184ef4626051
Trevor Manz <trevor.j.manz@gmail.com>
2023-06-22 13:04:48 -0400

    feat: Drop support for Python 3.7

--- anywidget/_version.py
+++ anywidget/_version.py
@@ -1,7 +1,4 @@
-try:
-    from importlib.metadata import PackageNotFoundError, version
-except ImportError:
-    from importlib_metadata import PackageNotFoundError, version  # type: ignore
+from importlib.metadata import PackageNotFoundError, version
 
 try:
     __version__ = version("anywidget")
