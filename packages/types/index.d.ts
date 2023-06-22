539c792fc8cc3b017aadddbc3cfe434b56c6b6c3
Trevor Manz <trevor.j.manz@gmail.com>
2023-06-22 10:14:18 -0400

    feat(types): infer event names from model

--- packages/types/index.d.ts
+++ packages/types/index.d.ts
@@ -1,5 +1,4 @@
 type MaybePromise<T> = T | Promise<T>;
-
 type ObjectHash = Record<string, any>;
 type CleanupFn = () => MaybePromise<void>;
 
@@ -10,20 +9,31 @@ interface EventHandler {
 	(...args: any[]): void;
 }
 
+/**
+ * Utility type to infer possible event names from a model.
+ *
+ * Autocomplete works for literal string unions, but adding a union
+ * of `string` negates autocomplete entirely. This is a workaround
+ * to provide autocomplete but still allow any string.
+ *
+ * @see https://github.com/microsoft/TypeScript/issues/29729
+ */
+type EventName<T extends PropertyKey> = `change:${T & string}` | 'msg:custom' | (string & {});
+
 export interface AnyModel<T extends ObjectHash = ObjectHash> {
 	get<K extends keyof T>(key: K): T[K];
 	set<K extends keyof T>(key: K, value: T[K]): void;
-	off(eventName?: string | null, callback?: EventHandler | null): void;
-	on(eventName: string, callback: EventHandler): void;
+	off<K extends keyof T>(eventName?: EventName<K> | null, callback?: EventHandler | null): void;
+	on<K extends keyof T>(eventName: EventName<K>, callback: EventHandler): void;
 	save_changes(): void;
 	send(content: any, callbacks?: any, buffers?: ArrayBuffer[] | ArrayBufferView[]): void;
 }
 
-export interface RenderContext<Model extends AnyModel> {
-	model: Model;
+export interface RenderContext<T extends ObjectHash = ObjectHash> {
+	model: AnyModel<T>;
 	el: HTMLElement;
 }
 
 export interface Render<T extends ObjectHash = ObjectHash> {
-	(context: RenderContext<AnyModel<T>>): MaybePromise<void | CleanupFn>;
+	(context: RenderContext<T>): MaybePromise<void | CleanupFn>;
 }
