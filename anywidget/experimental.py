39eadef4ad8f290ae60a1bff543ea50dccd722c7
Trevor Manz <trevor.j.manz@gmail.com>
2023-08-04 17:22:27 -0400

    feat: add dataclass decorator

--- anywidget/experimental.py
+++ anywidget/experimental.py
@@ -1,13 +1,18 @@
 from __future__ import annotations
 
+import dataclasses
 import pathlib
 import typing
+
+import psygnal
 
 from ._descriptor import MimeBundleDescriptor
 
 __all__ = ["widget", "MimeBundleDescriptor"]
 
-ModelT = typing.TypeVar("ModelT")
+_T = typing.TypeVar("_T")
+T = typing.TypeVar("T", bound="type[typing.Any]")
+F = typing.TypeVar("F", bound=typing.Callable)
 
 
 def widget(
@@ -15,7 +20,7 @@ def widget(
     esm: str | pathlib.Path,
     css: None | str | pathlib.Path = None,
     **kwargs: typing.Any,
-) -> typing.Callable[[ModelT], ModelT]:
+) -> typing.Callable[[T], T]:
     """Decorator to register a widget class as a mimebundle.
 
     Parameters
@@ -36,8 +41,71 @@ def widget(
     if css is not None:
         kwargs["_css"] = css
 
-    def _decorator(cls: ModelT) -> ModelT:
+    def _decorator(cls: _T) -> _T:
         setattr(cls, "_repr_mimebundle_", MimeBundleDescriptor(**kwargs))
         return cls
 
     return _decorator
+
+
+# To preserve the signature of the decorated class.
+# see: https://github.com/pyapp-kit/magicgui/blob/5e068f31eaeeb130f43c38727b25423cc3ea4de3/src/magicgui/schema/_guiclass.py#L145-L162
+def __dataclass_transform__(
+    *,
+    eq_default: bool = True,
+    order_default: bool = False,
+    kw_only_default: bool = False,
+    field_specifiers: tuple[type | typing.Callable[..., typing.Any], ...] = (()),
+) -> typing.Callable[[_T], _T]:
+    return lambda a: a
+
+
+@__dataclass_transform__(field_specifiers=(dataclasses.Field, dataclasses.field))
+def dataclass(
+    cls: T | None = None,
+    *,
+    esm: str | pathlib.Path,
+    css: None | str | pathlib.Path = None,
+    **dataclass_kwargs: typing.Any,
+) -> typing.Callable[[T], T]:
+    """Turns class into a dataclass, makes it evented, and registers it as a widget.
+
+    Parameters
+    ----------
+    cls : T | None
+        The class to decorate.
+    esm : str | pathlib.Path
+        The path or contents of an ES Module for the widget.
+    css : None | str | pathlib.Path, optional
+        The path or contents of a CSS file for the widget.
+    dataclass_kwargs : typing.Any
+        Additional keyword arguments to pass to the dataclass decorator.
+
+    Returns
+    -------
+    type
+        The evented dataclass.
+
+    Examples
+    --------
+    >>> @dataclass(esm="index.js")
+    ... class Counter:
+    ...     value: int = 0
+    ...
+    >>> counter = Counter()
+    >>> counter.value = 1
+    >>> counter
+    """
+
+    def _decorator(cls: T) -> T:
+        if dataclass_kwargs.get("frozen", False):
+            raise ValueError(
+                "The 'anywidget.experimental.dataclass' decorator does not support "
+                "dataclasses with `frozen=True`."
+            )
+        cls = dataclasses.dataclass(cls, **dataclass_kwargs)  # type: ignore
+        cls = psygnal.evented(cls)
+        cls = widget(esm=esm, css=css)(cls)
+        return cls
+
+    return _decorator(cls) if cls is not None else _decorator
--- tests/test_experimental.py
+++ tests/test_experimental.py
@@ -2,7 +2,7 @@ import dataclasses
 
 import psygnal
 from anywidget._descriptor import ReprMimeBundle
-from anywidget.experimental import MimeBundleDescriptor, widget
+from anywidget.experimental import MimeBundleDescriptor, dataclass, widget
 
 
 def test_decorator():
@@ -12,6 +12,19 @@ def test_decorator():
     @widget(esm=esm, css=css)
     @psygnal.evented
     @dataclasses.dataclass
+    class Foo:
+        bar: str = "baz"
+
+    foo = Foo()
+
+    assert isinstance(Foo._repr_mimebundle_, MimeBundleDescriptor)  # type: ignore
+    assert isinstance(foo._repr_mimebundle_, ReprMimeBundle)  # type: ignore
+
+def test_dataclass():
+    esm = "export function render({ model , el }) {}"
+    css = ".foo { color: red;}"
+
+    @dataclass(esm=esm, css=css)
     class Foo:
         bar: str = "baz"
 
