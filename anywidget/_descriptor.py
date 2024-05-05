b178d9584c853460f8830ba5a39bda05127ff018
Trevor Manz <trevor.j.manz@gmail.com>
2024-05-05 10:05:39 +0200

    chore: Increase mypy strictness

--- anywidget/_descriptor.py
+++ anywidget/_descriptor.py
@@ -366,7 +366,7 @@ class ReprMimeBundle:
         state, buffer_paths, buffers = remove_buffers(state)
         if getattr(self._comm, "kernel", None):
             msg = {"method": "update", "state": state, "buffer_paths": buffer_paths}
-            self._comm.send(data=msg, buffers=buffers)  # type: ignore
+            self._comm.send(data=msg, buffers=buffers)  # type: ignore[arg-type]
 
     def _handle_msg(self, msg: CommMessage) -> None:
         """Called when a msg is received from the front-end.
@@ -427,7 +427,7 @@ class ReprMimeBundle:
         """
         if js_to_py:
             # connect changes in the view to the instance
-            self._comm.on_msg(self._handle_msg)  # type: ignore
+            self._comm.on_msg(self._handle_msg)  # type: ignore[arg-type]
             self.send_state()
 
         if py_to_js and self._autodetect_observer:
