0aebfa5d223ab7f5e8eeb5b973d2948f2fadf1fc
Trevor Manz <trevor.j.manz@gmail.com>
2024-05-12 14:13:06 -0400

    fix: mypy typechecking

--- anywidget/_version.py
+++ anywidget/_version.py
@@ -12,7 +12,7 @@ except PackageNotFoundError:
     __version__ = "uninstalled"
 
 
-def get_semver_version(version: str):
+def get_semver_version(version: str) -> str:
     split = version.split(".", maxsplit=2)
     is_pre_release = "a" in split[2] or "b" in split[2]
     if is_pre_release:
