94a4b39793110e99a74f24e39226ea63a76239f8
Trevor Manz <trevor.j.manz@gmail.com>
2024-06-10 12:28:31 -0400

    Fix name

--- paper/export-pdf.ts
+++ paper/export-pdf.ts
@@ -5,6 +5,8 @@ let [frontMatter, raw] = Deno.readTextFileSync("paper.md")
 	.replace("---", "")
 	.split("---");
 
+let output = Deno.args[0] || "dist/paper.pdf";
+
 let data = parse(frontMatter);
 // biome-ignore lint/performance/noDelete: <explanation>
 delete data["affiliations"];
@@ -14,6 +16,19 @@ delete data["authors"];
 delete data["tags"];
 // biome-ignore lint/performance/noDelete: <explanation>
 delete data["date"];
+
+data["exports"] = {
+	format: "typst",
+	template: "lapreprint-typst",
+	output: output,
+};
+
+data["margin"] = [
+	{
+		"title": "Correspondence to",
+		"content": "Correspondence: [author-email]",
+	}
+]
 
 let markdown = raw
 	// Replace markdown images with myst images
@@ -33,10 +48,21 @@ ${content}
 	.replace("# Overview", "# Project Overview");
 
 Deno.writeTextFileSync(
-	"main.md",
+	"tmp.md",
 	`\
 ---
 ${stringify(data)}
 ---
 ${markdown}`,
 );
+
+new Deno.Command("myst", { args: ["build", "--pdf", "tmp.md"] })
+	.output()
+	.then((data) => {
+		console.log(data)
+		console.log(`PDF saved to ${output}`);
+	})
+	.catch(console.error)
+	.finally(async () => {
+		await Deno.remove("tmp.md");
+	});
