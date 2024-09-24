381d2ab129d21b3bf15e701b0054b9935d189cf3
Trevor Manz <trevor.j.manz@gmail.com>
2024-09-24 16:20:38 -0400

    chore: Add missing svg titles

--- docs/src/components/Header/Search.tsx
+++ docs/src/components/Header/Search.tsx
@@ -58,6 +58,7 @@ export default function Search() {
 					focusable="false"
 					role="img"
 				>
+					<title>Search</title>
 					<path
 						d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
 						stroke="currentColor"
